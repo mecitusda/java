347350ccbac1e81b26f9baf09cfc88c872328480
Abdul M. Usda <mirac_usda@hotmail.com>
2023-07-09 17:39:48 +0300

    day1

---
+++ src/dummycozum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class dummycozum {
+    public static void main(String [] args){
+
+        Scanner scanner=new Scanner(System.in);
+
+        int yas = scanner.nextInt();
+        scanner.nextLine();
+        String isim= scanner.nextLine();
+        System.out.println("yas: "+yas);
+        System.out.println("isim: "+isim);
+
+
+
+    }
+}
